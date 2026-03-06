42018062455477918dea576e0fdf8c83424070eb
NoizyNInja <reading.books4848@gmail.com>
2026-03-06 14:11:43 +1000

    Update service worker cache version to v16

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* sw.js - offline cache */
-const CACHE_NAME = "runsheet-v15";
+const CACHE_NAME = "runsheet-v16";
 
 const ASSETS = [
   "./",
@@ -69,6 +69,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
