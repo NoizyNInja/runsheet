f871c32160d90d341e73e378c29f6b13202bc010
NoizyNInja <reading.books4848@gmail.com>
2026-03-07 22:26:41 +1000

    Refactor sw.js for new cache version and cleanup
    Updated service worker to version 20 and simplified event listeners.

--- sw.js
+++ sw.js
@@ -1,83 +1,13 @@
-/* sw.js - offline cache */
-const CACHE_NAME = "runsheet-v16";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./sw.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
+const CACHE_NAME = "runsheet-v20";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    Promise.all([
-      self.clients.claim(),
-      caches.keys().then((keys) =>
-        Promise.all(
-          keys.map((key) => {
-            if (key !== CACHE_NAME) {
-              return caches.delete(key);
-            }
-          })
-        )
-      )
-    ])
-  );
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  // runtime cache for SheetJS CDN
-  if (url.hostname === "cdn.sheetjs.com") {
-    event.respondWith(
-      caches.match(req).then((cached) => {
-        if (cached) return cached;
-
-        return fetch(req).then((res) => {
-          const copy = res.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
-          return res;
-        });
-      })
-    );
-    return;
-  }
-
-  if (url.origin !== self.location.origin) return;
-
-  event.respondWith(
-    caches.match(req).then((cached) => {
-      if (cached) return cached;
-
-      return fetch(req).then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
-        return res;
-      });
-    })
-  );
+self.addEventListener("fetch", () => {
+  // Intentionally empty for now
 });
-
-
-
-
-
-
-
-
-
-
-
-
