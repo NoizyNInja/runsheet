bd356de68c80fb7b1c633fdb2ce86632fc94546d
NoizyNInja <reading.books4848@gmail.com>
2026-03-07 22:40:25 +1000

    Update CACHE_NAME to 'runsheet-v22'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "runsheet-v21";
+const CACHE_NAME = "runsheet-v22";
 
 self.addEventListener("install", () => {
   self.skipWaiting();
@@ -11,4 +11,5 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", () => {
   // Minimal SW for install testing
 });
+
 
